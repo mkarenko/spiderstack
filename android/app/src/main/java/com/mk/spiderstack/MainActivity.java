f20502eb92544869a9f4aca9dcea27e6da813bb5
m.karenko <m.karenko@outlook.com>
2024-06-01 12:02:58 +0200

    first commit

---
+++ android/app/src/main/java/com/mk/spiderstack/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mk.spiderstack;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mk.spiderstack',
+  appName: 'spiderstack',
+  webDir: 'dist'
+};
+
+export default config;
